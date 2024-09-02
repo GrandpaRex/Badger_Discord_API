cb10c84f4bd92e2fcebbe76ac6de7009f725a7aa
GrandpaRex <77104671+GrandpaRex@users.noreply.github.com>
2024-09-02 00:29:54 -0500

    Update server.lua
    Per requested changes

--- server.lua
+++ server.lua
@@ -21,12 +21,14 @@ while resourceState ~= "started" do
 		print('[Badger_Discord_API] The resource took too long to start!')
 		return
 	end
+	print("[Badger_Discord_API] For support, make sure to join Badger's official Discord server: discord.gg/WjB5VFz");
+	print('^7[^2Zap-Hosting^7] ^3Use code ^5TheWolfBadger-4765 ^3at checkout for ^220% ^3off of selected services. Visit ^5https://zap-hosting.com/badger ^3to get started!');
 end
 
 if GetCurrentResourceName() ~= "Badger_Discord_API" then
 	print("[" .. GetCurrentResourceName() .. "] " .. "IMPORTANT: This resource must be named Badger_Discord_API for it to work properly with other scripts...");
-	StopResource(GetCurrentResourceName())
 	print("[" .. GetCurrentResourceName() .. "] " .. "Resource stopped!");
+	StopResource(GetCurrentResourceName())
 end
 
 function DiscordRequest(method, endpoint, jsondata, reason)
@@ -71,10 +73,17 @@ tracked = {}
 
 RegisterNetEvent('Badger_Discord_API:PlayerLoaded')
 AddEventHandler('Badger_Discord_API:PlayerLoaded', function()
-	local license = GetIdentifier(source, 'license');
+	if (GetCurrentResourceName() ~= "Badger_Discord_API") then 
+		TriggerClientEvent('chatMessage', -1, '^1[^5SCRIPT ERROR^1] ^3The script ^1' .. GetCurrentResourceName() .. ' ^3will not work properly... You must '
+	.. 'rename the resource to ^1Badger_Discord_API');
+	end
 	if (tracked[license] == nil) then 
 		tracked[license] = true;
+		TriggerClientEvent('chatMessage', source, 
+		'^1[^5Badger_Discord_API^1] ^3The Discord API script was created by Badger. You may join his Discord at: ^6discord.gg/WjB5VFz')
 	end
+	TriggerClientEvent('chatMessage', source, 
+		'^7[^2Zap-Hosting^7] ^3Use code ^5TheWolfBadger-4765 ^3at checkout for ^220% ^3off of selected services. Visit ^5https://zap-hosting.com/badger ^3to get started!');
 end)
 
 card = '{"type":"AdaptiveCard","$schema":"http://adaptivecards.io/schemas/adaptive-card.json","version":"1.4","body":[{"type":"Image","url":"' .. Config.Splash.Header_IMG .. '","horizontalAlignment":"Center"},{"type":"Container","items":[{"type":"TextBlock","text":"Badger_Discord_API","wrap":true,"fontType":"Default","size":"ExtraLarge","weight":"Bolder","color":"Light","horizontalAlignment":"Center"},{"type":"TextBlock","text":"' .. Config.Splash.Heading1 .. '","wrap":true,"size":"Large","weight":"Bolder","color":"Light","horizontalAlignment":"Center"},{"type":"TextBlock","text":"' .. Config.Splash.Heading2 .. '","wrap":true,"color":"Light","size":"Medium","horizontalAlignment":"Center"},{"type":"ColumnSet","height":"stretch","minHeight":"100px","bleed":true,"horizontalAlignment":"Center","columns":[{"type":"Column","width":"stretch","items":[{"type":"ActionSet","actions":[{"type":"Action.OpenUrl","title":"Discord","url":"' .. Config.Splash.Discord_Link .. '","style":"positive"}],"horizontalAlignment":"Right"}],"height":"stretch"},{"type":"Column","width":"stretch","items":[{"type":"ActionSet","actions":[{"type":"Action.OpenUrl","title":"Website","style":"positive","url":"' .. Config.Splash.Website_Link .. '"}],"horizontalAlignment":"Left"}]}]},{"type":"ActionSet","actions":[{"type":"Action.OpenUrl","title":"Click to join Badger\'s Discord","style":"destructive","iconUrl":"https://i.gyazo.com/0904b936e8e30d0104dec44924bd2294.gif","url":"https://discord.com/invite/WjB5VFz"}],"horizontalAlignment":"Center"}],"style":"default","bleed":true,"height":"stretch"},{"type":"Image","url":"https://i.gyazo.com/7e896862b14be754ae8bad90b664a350.png","selectAction":{"type":"Action.OpenUrl","url":"https://zap-hosting.com/badger"},"horizontalAlignment":"Center"}]}'
@@ -514,6 +523,7 @@ function GetUserRolesInGuild (user, guild)
 		return roles
 	elseif member.code == 404 then
 		-- This code is ignored here because if a user isn't a member of a guild it throws an error that doesn't mean anything
+		sendDebugMessage("[Badger_Discord_API] The user was not a member of one of your guilds, this is safe to ignore.")
 		Wait(1)
 	else
 		sendDebugMessage("[Badger_Discord_API] ERROR: Code 200 was not reached... Returning false. [Member Data NOT FOUND] DETAILS: " .. error_codes_defined[member.code])
